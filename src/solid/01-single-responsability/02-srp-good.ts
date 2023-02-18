c9e314e53e44ea2748848f8855840342395fb998
jose david lombana <jose4125@gmail.com>
2023-02-18 16:15:49 +0000

    split srp files

--- src/solid/01-single-responsability/02-srp-good.ts
+++ src/solid/01-single-responsability/02-srp-good.ts
@@ -1,6 +1,5 @@
 import * as toastr from 'toastr'
 
-// good
 (() => {
 
   class ErrorHandler {
@@ -32,50 +31,6 @@ import * as toastr from 'toastr'
       }).then(response => {
         if (!response.ok) {
           ErrorHandler.handle(response)
-        }
-
-        toastr.success('yeiiiiii!!!!')
-        return response.json()
-      })
-    }
-
-  }
-
-  const http = new HttpClient();
-  // http.get('https://httpstat.us/500').then(data => {
-  //   console.log('data', data)
-  // })
-  http.get('https://jsonplaceholder.typicode.com/posts').then(data => {
-    console.log('data', data)
-  })
-})();
-
-// bad
-(() => {
-
-  class HttpClient {
-
-    get (url: string) {
-      return fetch(url, {
-        headers: {
-          'Accept': 'application/json'
-        }
-      }).then(response => {
-        if (!response.ok) {
-          if(response.status === 401) {
-            toastr.error('you are not authorized to view this content')
-            return
-          }
-          if(response.status === 404) {
-            toastr.waning('content not found')
-            return
-          }
-          if(response.status === 500) {
-            toastr.error('internal server error')
-            return
-          }
-          toastr.info('unknown error')
-          return
         }
 
         toastr.success('yeiiiiii!!!!')
